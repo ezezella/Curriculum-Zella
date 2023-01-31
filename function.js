38b8114ddd99adfa63797430f33829c445533000
Ezequiel Zella <ezella@circostudio.com>
2023-01-31 01:11:10 -0300

    CV terminado

---
+++ function.js
@@ -0,0 +1,5 @@
+document.getElementById('boton').onclick = function () {
+    document.getElementById('Lenguajes').innerHTML = "<li>C avanzado</li><li>C++ avanzado</li><li>Javascript avanzado</li><li>Typescript intermedio</li><li>Java intermedio</li><li>PHP intermedio</li><li>SQL intermedio</li><li>Angular Basico</li><li>HTML Basico</li><li>Css Basico</li>";
+    document.getElementById('container').style.height = "1110px";
+    
+}
